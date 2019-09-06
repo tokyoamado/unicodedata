fa040378c7064719ef5ae7d0a136bce93761ab30
tokyoamado <limgcamo@gmail.com>
2019-09-06 15:59:57 +0900

    Implement name functions

--- src/Data/Text/Ucd/Char.hs
+++ src/Data/Text/Ucd/Char.hs
@@ -7,8 +7,9 @@ module Data.Text.Ucd.Char
   , charsOf
   ) where
 
-import Data.Char (chr, ord, toUpper)
+import Data.Char (chr, ord, toLower, toUpper)
 import Language.Haskell.TH (runIO)
+import Numeric (showHex)
 import qualified Data.IntMap as I
 import qualified Data.Map.Strict as M
 import qualified Data.Text as T
@@ -16,11 +17,26 @@ import qualified Data.Text.IO as T
 
 import Data.Text.Ucd.Internal.Parser
 
-nameOf :: Char -> String
-nameOf c = "undefined"
+cpStr :: Char -> String
+cpStr c = drop d "000" ++ raw
+  where
+    raw = map toUpper $ showHex (ord c) ""
+    len = length raw
+    d = if len <= 4 then len - 1 else len - 3
 
-charOf :: String -> Char
-charOf s = 'u'
+nameOf :: Char -> String
+nameOf c = case I.lookupLE (ord c) nameDic of
+  Just (_, Single cp n) -> if ord c == cp then n else "U+" ++ cpStr c
+  Just (_, Range b e n) -> if ord c <= e then init n ++ cpStr c else "U+" ++ cpStr c
+  Nothing               -> "U+" ++ cpStr c
+
+charOf :: String -> Maybe Char
+charOf s = case M.lookup (norm s) nameDicRev of
+  Just (Single cp _) -> Just $ chr cp
+  Just (Range b e n) -> Nothing  -- Don't support range.
+  Nothing -> Nothing
+  where
+    norm = map toUpper . filter (\c -> c `notElem` (" -_" :: String))
 
 blockOf :: Char -> String
 blockOf c = case I.lookupLE (ord c) blockDic of
@@ -32,11 +48,18 @@ charsOf block = case M.lookup n blockDicRev of
   Just (b, e) -> [b .. e]
   Nothing     -> []
   where
-    n = (map toUpper . filter (\c -> c `notElem` (" -_" :: String))) block
+    n = (map toLower . filter (\c -> c `notElem` (" -_" :: String))) block
 
 (blockDic, blockDicRev) = $(do
-  let norm = T.toUpper . T.filter (\c -> T.all (/= c) " -_")
-  blocks_txt <- runIO $ map parseLine . filter (\s -> s /= "" && T.head s /= '#') . map T.strip . T.lines <$> T.readFile "ucd/Blocks.txt"
+  let norm = T.toLower . T.filter (\c -> T.all (/= c) " -_")
+  blocks_txt <- runIO $ map parseBlock . filter (\s -> s /= "" && T.head s /= '#') . map T.strip . T.lines <$> T.readFile "ucd/Blocks.txt"
   let (dic, dicRev) = unzip $ map (\(b, e, n) -> ((b, (e, T.unpack n)), ((T.unpack . norm) n, (chr b, chr e)))) blocks_txt
+  [| (I.fromList dic, M.fromList dicRev) |]
+  )
+
+(nameDic, nameDicRev) = $(do
+  let norm = map toUpper . filter (\c -> c `notElem` (" -_" :: String))
+  name_txt <- runIO $ map parseName . filter (\s -> s /= "" && T.head s /= '#') . map T.strip . T.lines <$> T.readFile "ucd/extracted/DerivedName.txt"
+  let (dic, dicRev) = unzip $ map (\name -> ((nameIdx name, name), ((norm . nameText) name, name))) name_txt
   [| (I.fromList dic, M.fromList dicRev) |]
   )
--- src/Data/Text/Ucd/Internal/Parser.hs
+++ src/Data/Text/Ucd/Internal/Parser.hs
@@ -1,22 +1,51 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, DeriveLift #-}
 
-module Data.Text.Ucd.Internal.Parser where
+module Data.Text.Ucd.Internal.Parser
+  ( Name (Single, Range)
+  , nameIdx
+  , nameText
+  , parseBlock
+  , parseName
+  ) where
 
-import Data.Attoparsec.Text (Parser, char, hexadecimal, parseOnly, skipSpace, string, takeText)
+import Data.Attoparsec.Text (Parser, char, hexadecimal, option, parseOnly, skipSpace, string, takeText)
+import Language.Haskell.TH.Syntax (Lift)
 import qualified Data.Text as T
 
-parseLine :: T.Text -> (Int, Int, T.Text)
-parseLine t = case parseOnly line t of
+parseBlock :: T.Text -> (Int, Int, T.Text)
+parseBlock t = case parseOnly blockline t of
   Right (begin, end, text) -> (begin, end, text)
   Left err                 -> error err
 
-line :: Parser (Int, Int, T.Text)
-line = do
+blockline :: Parser (Int, Int, T.Text)
+blockline = do
   begin <- hexadecimal
   string ".."
   end <- hexadecimal
   char ';'
   skipSpace
   name <- takeText
-  skipSpace
   return (begin, end, name)
+
+data Name = Single Int String | Range Int Int String deriving (Lift, Show)
+nameIdx :: Name -> Int
+nameIdx (Single i _)  = i
+nameIdx (Range i _ _) = i
+nameText :: Name -> String
+nameText (Single _ n)  = n
+nameText (Range _ _ n) = n
+
+parseName :: T.Text -> Name
+parseName t = case parseOnly nameline t of
+  Right name -> name
+  Left err   -> error err
+
+nameline :: Parser Name
+nameline = do
+  begin <- hexadecimal
+  end <- option (-1) $ string ".." *> hexadecimal
+  skipSpace
+  char ';'
+  skipSpace
+  name <- takeText
+  return $ if end == (-1) then Single begin (T.unpack name) else Range begin end (T.unpack name)
